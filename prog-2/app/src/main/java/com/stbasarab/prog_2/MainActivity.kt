bfa7327c7dbb252f8a3df7d873a9e53a455b1366
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-12-08 23:36:42 +0200

    Implemented prog-2

--- prog-2/app/src/main/java/com/stbasarab/prog_2/MainActivity.kt
+++ prog-2/app/src/main/java/com/stbasarab/prog_2/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.stbasarab.prog_2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-  private var vector: DoubleArray? = null
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     val vector = getVector()
+    showVectors(vector)
   }
 
   override fun onNewIntent(intent: Intent?) {
     super.onNewIntent(intent)
+    setIntent(intent)
     val vector = getVector()
+    showVectors(vector)
   }
 
   private fun getVector(): DoubleArray {
@@ -28,7 +30,18 @@ class MainActivity : AppCompatActivity() {
     return DoubleArray(n) { random.nextDouble(min, max) }
   }
 
-  private fun showVector(vector: DoubleArray) {
+  private fun showVectorBox(linearLayout: LinearLayout, text: String) {
+    for (i in 0 until linearLayout.childCount) {
+      val textView = linearLayout.getChildAt(i) as TextView
+      textView.text = text
+    }
+  }
 
+  private fun showVectors(vector: DoubleArray) {
+    val formattedVector = vector.map { "%.2f".format(it) }
+    val verticalVector = formattedVector.joinToString("\n")
+    val horizontalVector = formattedVector.joinToString(" ")
+    showVectorBox(findViewById(R.id.top_box), verticalVector)
+    showVectorBox(findViewById(R.id.bottom_box), horizontalVector)
   }
 }
